4e496dbb74c2af658f341ed0a1f74326f36c5d62
Rene Sanchez <renesanchez@yahoo.com>
2017-03-08 22:18:27 -0600

    base.js for ready folder changed to 'html' method

--- starter-code/ready/base.js
+++ starter-code/ready/base.js
@@ -2,11 +2,13 @@
   What needs to change so that the page says "Go!"?
 */
 
+
+
 $(document).ready(function(){
-  console.log( "The page says:", $("h1").text() );
+  console.log( "The page says:", $("h1").html("Go!") );
 });
 
-$("h1").text("Go!");
+$("h1").text();
 
 if ( $("h1").text() === "Go!" ){
   console.log( "The page says:", $("h1").text() );
