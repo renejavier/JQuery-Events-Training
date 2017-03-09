07029f5cb26b519cc2b9bdff8e636e85aa39f043
Rene Sanchez <renesanchez@yahoo.com>
2017-03-08 23:16:41 -0600

    base.js for change add two numbers completed?

--- starter-code/change/base.js
+++ starter-code/change/base.js
@@ -4,4 +4,21 @@ $(document).ready(function(){
 
   $("#time").text( Date.now() );
 
+  // function compute() {
+  //   if ( $('input[name=type]:checked').val() != undefined ) {
+  //       var a = parseInt($('#left').val());
+  //       var b = parseInt($('#right').val());
+  //       var total = a+b;
+  //       $('#total').val(a+b);
+  //   }
+
+  $("input").keyup(function(){
+  var a = parseInt($('#left').val());
+    var b = parseInt($('#right').val());
+        total = a + b;
+    $('#total').val(total);
+
+})
+
+
 })
